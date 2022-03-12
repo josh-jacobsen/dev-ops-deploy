52529f27713a6e3746fedb0af737ee004bae1807
Josh Jacobsen <josh.jacobsen@vitaler.com>
2022-03-12 22:37:58 +1300

    Basic working code

--- src/release-retention.test.ts
+++ src/release-retention.test.ts
@@ -1,18 +1,17 @@
 import {
   calculateReleasesToRetain,
   Deployment,
-  filterReleasesWithNoDeployments,
+  filterReleasesWithNoDeploymentsOrProjects,
+  Project,
   Release,
 } from './release-retention'
 
 describe('CalculateReleasesToRetain', () => {
-  describe('Filter releases with no deployments', () => {
-    it('Returns releases that have at least one deployment', () => {
-      const releases: Release[] = [
+    const releases: Release[] = [
         {
           Id: 'Release-1',
           ProjectId: 'Project-1',
-          Version: null,
+          Version: '1.0.1',
           Created: '2000-01-02T13:00:00',
         },
       ]
@@ -25,79 +24,89 @@ describe('CalculateReleasesToRetain', () => {
           DeployedAt: '2000-01-01T10:00:00',
         },
       ]
-
-      const filteredReleases = filterReleasesWithNoDeployments(
+      const projects: Project[] = [
+        {
+          Id: 'Project-1',
+          Name: 'Random Quotes',
+        },
+      ]
+  describe('Filter releases with no deployments', () => {
+      
+    it('Returns releases that have at least one deployment', () => {
+      const filteredReleases = filterReleasesWithNoDeploymentsOrProjects(
         releases,
-        deployments
+        deployments,
+        projects
       )
 
       expect(filteredReleases).toEqual([
         {
           Id: 'Release-1',
           ProjectId: 'Project-1',
-          Version: null,
+          Version: '1.0.1',
           Created: '2000-01-02T13:00:00',
         },
       ])
     })
 
     it('Returns multiple releases that have at least one deployment', () => {
-        const releases: Release[] = [
-          {
-            Id: 'Release-1',
-            ProjectId: 'Project-1',
-            Version: null,
-            Created: '2000-01-02T13:00:00',
-          },
-          {
-            Id: 'Release-2',
-            ProjectId: 'Project-1',
-            Version: null,
-            Created: '2000-01-02T13:00:00',
-          },
-        ]
-  
-        const deployments: Deployment[] = [
-          {
-            Id: 'Deployment-1',
-            ReleaseId: 'Release-1',
-            EnvironmentId: 'Environment-1',
-            DeployedAt: '2000-01-01T10:00:00',
-          },
-          {
-            Id: 'Deployment-2',
-            ReleaseId: 'Release-2',
-            EnvironmentId: 'Environment-1',
-            DeployedAt: '2000-01-01T10:00:00',
-          },
-        ]
-  
-        const filteredReleases = filterReleasesWithNoDeployments(
-          releases,
-          deployments
-        )
-  
-        expect(filteredReleases).toEqual([
-            {
-                Id: 'Release-1',
-                ProjectId: 'Project-1',
-                Version: null,
-                Created: '2000-01-02T13:00:00',
-              },
-              {
-                Id: 'Release-2',
-                ProjectId: 'Project-1',
-                Version: null,
-                Created: '2000-01-02T13:00:00',
-              },
-        ])
-      })
+      const releases: Release[] = [
+        {
+          Id: 'Release-1',
+          ProjectId: 'Project-1',
+          Version: '1.0.1',
+          Created: '2000-01-02T13:00:00',
+        },
+        {
+          Id: 'Release-2',
+          ProjectId: 'Project-1',
+          Version: '1.0.1',
+          Created: '2000-01-02T13:00:00',
+        },
+      ]
+
+      const deployments: Deployment[] = [
+        {
+          Id: 'Deployment-1',
+          ReleaseId: 'Release-1',
+          EnvironmentId: 'Environment-1',
+          DeployedAt: '2000-01-01T10:00:00',
+        },
+        {
+          Id: 'Deployment-2',
+          ReleaseId: 'Release-2',
+          EnvironmentId: 'Environment-1',
+          DeployedAt: '2000-01-01T10:00:00',
+        },
+      ]
+
+      const filteredReleases = filterReleasesWithNoDeploymentsOrProjects(
+        releases,
+        deployments,
+        projects
+      )
+
+      expect(filteredReleases).toEqual([
+        {
+          Id: 'Release-1',
+          ProjectId: 'Project-1',
+          Version: '1.0.1',
+          Created: '2000-01-02T13:00:00',
+        },
+        {
+          Id: 'Release-2',
+          ProjectId: 'Project-1',
+          Version: '1.0.1',
+          Created: '2000-01-02T13:00:00',
+        },
+      ])
+    })
     it('Remove releases with no deployments', () => {
       const releases: Release[] = [
         {
           Id: 'Release-3',
           ProjectId: 'Project-1',
-          Version: null,
+          Version: '1.0.1',
           Created: '2000-01-02T13:00:00',
         },
       ]
@@ -111,9 +120,10 @@ describe('CalculateReleasesToRetain', () => {
         },
       ]
 
-      const filteredReleases = filterReleasesWithNoDeployments(
+      const filteredReleases = filterReleasesWithNoDeploymentsOrProjects(
         releases,
-        deployments
+        deployments,
+        projects
       )
 
       expect(filteredReleases).toEqual([])
@@ -125,7 +135,7 @@ describe('CalculateReleasesToRetain', () => {
         {
           Id: 'Release-3',
           ProjectId: 'Project-1',
-          Version: null,
+          Version: '1.0.1',
           Created: '2000-01-02T13:00:00',
         },
       ]
@@ -138,16 +148,62 @@ describe('CalculateReleasesToRetain', () => {
           DeployedAt: '2000-01-01T10:00:00',
         },
       ]
-      const retainedReleases = calculateReleasesToRetain(releases, deployments)
+      const retainedReleases = calculateReleasesToRetain(
+        releases,
+        deployments,
+        projects
+      )
       expect(retainedReleases).toEqual([
         {
-          Id: 'Release-3',
           ProjectId: 'Project-1',
-          Version: null,
-          Created: '2000-01-02T13:00:00',
+          EnvironmentId: 'Environment-1',
+          Releases: [{Id: 'Release-3', Version: '1.0.1', DeployedAt: '2000-01-01T10:00:00'}]
         },
       ])
     })
 
+    it('should return multiple releases for each project and environment combination', () => {
+        const releases: Release[] = [
+          {
+            Id: 'Release-3',
+            ProjectId: 'Project-1',
+            Version: '1.0.1',
+            Created: '2000-01-02T13:00:00',
+          },
+          {
+            Id: 'Release-4',
+            ProjectId: 'Project-1',
+            Version: '1.0.2',
+            Created: '2000-01-03T10:00:00',
+          },
+        ]
+  
+        const deployments: Deployment[] = [
+          {
+            Id: 'Deployment-1',
+            ReleaseId: 'Release-3',
+            EnvironmentId: 'Environment-1',
+            DeployedAt: '2000-01-01T10:00:00',
+          },
+          {
+            Id: 'Deployment-2',
+            ReleaseId: 'Release-4',
+            EnvironmentId: 'Environment-1',
+            DeployedAt: '2000-01-03T13:00:00',
+          },
+        ]
+        const retainedReleases = calculateReleasesToRetain(
+          releases,
+          deployments,
+          projects
+        )
+        expect(retainedReleases).toEqual([
+          {
+            ProjectId: 'Project-1',
+            EnvironmentId: 'Environment-1',
+            Releases: [{Id: 'Release-3', Version: '1.0.1', DeployedAt: '2000-01-01T10:00:00'}, {Id: 'Release-4', Version: '1.0.2', DeployedAt: '2000-01-03T13:00:00'}]
+          },
+        ])
+      })
   })
 })
--- src/release-retention.ts
+++ src/release-retention.ts
@@ -14,23 +14,73 @@ export interface Deployment {
   DeployedAt: string
 }
 
+export interface Project {
+  Id: string
+  Name: string
+}
+
+export interface DeployedRelease {
+  Id: string
+  Version?: string | null
+  DeployedAt: string
+}
+
+export interface ReleaseRetention {
+  ProjectId: string
+  EnvironmentId: string
+  Releases: DeployedRelease[]
+}
+
+
 export async function readFile(path: string) {
   const a = await fs.readFile(path, 'utf-8')
   const things: Release[] = JSON.parse(a)
   console.log(things)
 }
 
-export function filterReleasesWithNoDeployments(releases: Release[], deployments: Deployment[]) {
-  return releases.filter(release => {
+export function filterReleasesWithNoDeploymentsOrProjects(releases: Release[], deployments: Deployment[], projects: Project[]) {
+  const releasesWithDeployments = releases.filter(release => {
     if (deployments.some(deployment => deployment.ReleaseId === release.Id)){
       return true
     }
     return false
   })
+
+  // I'm assuming that if a release is associated with a Project that isn't in the Projects.json file, 
+  // then this release should be excluded from the results.
+  const releasesWithProjects = releasesWithDeployments.filter(release => {
+    if (projects.some(project => project.Id === release.ProjectId)) {
+      return true
+    }
+    return false
+  })
+
+  return releasesWithProjects
 }
 
-export function calculateReleasesToRetain(releases: Release[], deployments: Deployment[]): Release[] {
-  const filteredReleases = filterReleasesWithNoDeployments(releases, deployments)
+export function calculateReleasesToRetain(releases: Release[], deployments: Deployment[], projects: Project[]): ReleaseRetention[] {
+  const filteredReleases = filterReleasesWithNoDeploymentsOrProjects(releases, deployments, projects)
 
-  return filteredReleases
+  const releaseRetentionsToReturn: ReleaseRetention[] = []
+
+  filteredReleases.map(release => {
+    const deploymentsToLookAt = deployments.filter(deploy => deploy.ReleaseId === release.Id)
+
+    deploymentsToLookAt.map(deploy => {
+      const check = releaseRetentionsToReturn.find(thing => thing.EnvironmentId === deploy.EnvironmentId && thing.ProjectId === release.ProjectId)
+      if (check) {
+        check.Releases.push({Id: release.Id, Version: release.Version, DeployedAt: deploy.DeployedAt})
+      } else {
+        releaseRetentionsToReturn.push({EnvironmentId: deploy.EnvironmentId, ProjectId: release.ProjectId, Releases: [{Id: release.Id, Version: release.Version, DeployedAt: deploy.DeployedAt}]})
+      }
+    })
+  })
+
+
+
+  // iterate through the releases. 
+  // compare to Deployments.json
+  // generate new object 
+  // ReleaseRetention []
+  return releaseRetentionsToReturn
 }
